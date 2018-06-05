1e853fc637183a9c3fa52b1efcc608abc04ed66a
Kayvan Vakili <kvakili813@gmail.com>
2018-06-05 18:56:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,6 @@ class Triangle
   if side_1 * side_2 * side_3 == 0 || side_1 >= (side_2 + side_3) || side_2 >= (side_1 + side_3) || side_3 >= (side_2 + side_1)
     begin
       raise TriangleError
-      puts
     end
   elsif (side_1 == side_2) && (side_2 == side_3)
       :equilateral
